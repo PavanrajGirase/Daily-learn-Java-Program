777e0f641ef21ce7b21a9167d096f913c3e0993a
Pavanraj Girase <139738835+PavanrajGirase@users.noreply.github.com>
2025-02-09 12:32:15 +0530

    Variable used Program

---
+++ variableuse.java
@@ -0,0 +1,11 @@
+public class variableuse {
+    public static void main(String[] args) {
+        // int = is data type
+        // a = variable name/memroy location name
+        // = is operator
+        // 10 = is value
+        int a=10;
+        // a is display value
+        System.out.println(a);
+    }
+}
