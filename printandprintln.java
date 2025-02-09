0a8d5341b0b106d4e0e243d63864808e16b1fe7f
Pavanraj Girase <139738835+PavanrajGirase@users.noreply.github.com>
2025-02-09 12:30:53 +0530

    Print And Println Program

---
+++ printandprintln.java
@@ -0,0 +1,8 @@
+public class printandprintln {
+    public static void main(String[] args){
+        //Print used to simple hello world print but next print are not show this new line.
+        System.out.print("Hello, World \n");
+        //Println are used to automatic new line println.
+        System.out.println("Hello, World"); 
+    }
+}
