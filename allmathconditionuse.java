a24d81729e85ed0f44fc18ae232349da93cc8300
Pavanraj Girase <139738835+PavanrajGirase@users.noreply.github.com>
2025-02-09 12:53:46 +0530

    All math conditions use(+,-,*,/).

---
+++ allmathconditionuse.java
@@ -0,0 +1,16 @@
+public class allmathconditionuse {
+    public static void main(String[]args)
+    {
+        int a,b,sum,sub,mul,div;
+        a=10;
+        b=20;
+        sum=a+b;
+        sub=a-b;
+        mul=a*b;
+        div=a/b;
+        System.out.println(sum);
+        System.out.println(sub);
+        System.out.println(mul);
+        System.out.println(div);
+    }
+}
